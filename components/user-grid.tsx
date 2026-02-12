d9cf72a1ba939359b34e1c1765f60efe3bb9a1c2
Hemanta Sundaray <pgcim14.hemant@spjimr.org>
2026-02-12 20:51:09 +0530

    User Grid Updated

--- components/user-grid.tsx
+++ components/user-grid.tsx
@@ -1,14 +1,6 @@
 "use client";
 
-import { Result, useAtomValue } from "@effect-atom/atom-react";
-
-import { FailureCard } from "@/components/failure-card";
-import {
-  getNoUsersFoundReason,
-  UserEmptyCard,
-} from "@/components/user-empty-card";
-import { UserGridSpinner } from "@/components/user-grid-spinner";
-import { UserSuccessCard } from "@/components/user-success-card";
+import { useAtomValue } from "@effect-atom/atom-react";
 
 export function UserGrid() {
   return <h1>User Grid</h1>;
