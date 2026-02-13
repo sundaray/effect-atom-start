0a6db200295944d30627b5abfde13a300b3e943a
Hemanta Sundaray <pgcim14.hemant@spjimr.org>
2026-02-13 12:05:56 +0530

    Update Root Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
 "use client";
 
-import { Suspense } from "react";
 import { Link } from "react-transition-progress/next";
 
 import { UserGrid } from "@/components/user-grid";
-import { UserGridSpinner } from "@/components/user-grid-spinner";
-import { UserGridSuspense } from "@/components/user-grid-suspense";
 import { UserPagination } from "@/components/user-pagination";
 import { UserSearchBar } from "@/components/user-search-bar";
 
@@ -16,7 +13,7 @@ export default function HomePage() {
         <h1>Users</h1>
         <Link
           href="/add-user"
-          className="bg-accent text-foreground hover:bg-neutral-200 px-4 py-2 text-sm font-medium transition-colors"
+          className="bg-accent text-foreground px-4 py-2 text-sm font-medium transition-colors hover:bg-neutral-200"
         >
           + Add User
         </Link>
