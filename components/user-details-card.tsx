80c6db1ead669a897cb83cbdc1c5d2130f3579ff
Hemanta Sundaray <pgcim14.hemant@spjimr.org>
2026-02-14 09:50:51 +0530

    Updated User Details Component

--- components/user-details-card.tsx
+++ components/user-details-card.tsx
@@ -10,26 +10,23 @@ interface UserDetailsCardProps {
 
 export function UserDetailsCard({ user, waiting }: UserDetailsCardProps) {
   return (
-    <div
-      className={cn(
-        "bg-white border p-6 flex flex-col justify-between gap-6 relative container max-w-md",
-        waiting && "opacity-50 pointer-events-none",
-      )}
-    >
-      <div className="space-y-2">
-        <h3 className="font-semibold text-2xl">
+    <div className="relative container flex max-w-md flex-col justify-between gap-6 border bg-white p-6">
+      <div
+        className={cn("space-y-2", waiting && "pointer-events-none opacity-50")}
+      >
+        <h3 className="text-2xl font-semibold">
           {user.firstName} {user.lastName}
         </h3>
-        <p className="text-base text-neutral-600 font-medium">
+        <p className="text-base font-medium text-neutral-600">
           {user.company.title}
         </p>
         <p className="text-sm text-neutral-500">{user.company.name}</p>
       </div>
 
-      <p className="text-sm text-neutral-500 break-all">{user.email}</p>
+      <p className="text-sm break-all text-neutral-500">{user.email}</p>
 
-      <div className="flex items-start gap-3 bg-neutral-50 p-3 border">
-        <Icons.mapPin className="size-5 text-neutral-400 mt-0.5 shrink-0" />
+      <div className="flex items-start gap-3 border bg-neutral-50 p-3">
+        <Icons.mapPin className="mt-0.5 size-5 shrink-0 text-neutral-400" />
         <div className="text-sm text-neutral-600">
           <p>{user.address.address}</p>
           <p>
