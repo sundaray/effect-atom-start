0fcd3122d58d994074ffa8a63235e5fd03ea7a2a
Hemanta Sundaray <pgcim14.hemant@spjimr.org>
2026-02-16 17:44:52 +0530

    Updated Main

--- components/user-pagination.tsx
+++ components/user-pagination.tsx
@@ -1,12 +1,5 @@
 "use client";
 
-import { Result, useAtom, useAtomValue } from "@effect-atom/atom-react";
-
-import { Icons } from "@/components/icons";
-import { Button } from "@/components/ui/button";
-
-import { USERS_PER_PAGE } from "@/lib/constants";
-
 export function UserPagination() {
   return <h1>User Pagination</h1>;
 }
