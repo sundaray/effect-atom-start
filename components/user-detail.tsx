89a607b02ff09cee756b9a35a228d4a310ff00f4
Hemanta Sundaray <pgcim14.hemant@spjimr.org>
2026-02-16 14:11:59 +0530

    Fixed User Detail Component

--- components/user-detail.tsx
+++ components/user-detail.tsx
@@ -1,14 +1,5 @@
 "use client";
 
-import { useParams } from "next/navigation";
-import { Result, useAtomValue } from "@effect-atom/atom-react";
-
-import { FailureCard } from "@/components/failure-card";
-import { UserDetailsCard } from "@/components/user-details-card";
-import { UserGridSpinner } from "@/components/user-grid-spinner";
-
-import { getErrorInfo } from "@/lib/utils";
-
 export function UserDetail() {
   return <h1>User Detail</h1>;
 }
